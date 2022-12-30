684615fc14571d417bfa154be28a8ba074af8dd4
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-12-30 17:08:38 +0200

    add note viewModel and "hive package" for database.

---
+++ lib/models/note_model.dart
@@ -0,0 +1,14 @@
+class NoteModel {
+  final String title;
+  final String subTitle;
+  final String date;
+  final String category;
+  final int color;
+
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.category,
+      required this.color});
+}
